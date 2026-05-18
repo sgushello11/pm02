5482b82c7c6075450d661f0472d958f5adad1646
Gush <loveg6448@gmail.com>
2026-05-18 14:42:43 +0300

    .

--- Сессия 6/OperatorModule.Tests/obj/Debug/net8.0-windows/OperatorModule.Tests.AssemblyInfo.cs
+++ Сессия 6/OperatorModule.Tests/obj/Debug/net8.0-windows/OperatorModule.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OperatorModule.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b2a0a102a76e0ab2edfb87456232c8f9b15819")]
 [assembly: System.Reflection.AssemblyProductAttribute("OperatorModule.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OperatorModule.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
